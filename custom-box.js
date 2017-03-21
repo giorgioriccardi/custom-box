4c503d9ef012528c07c95ce343f6c18af81e1cc5
Giorgio Riccardi <griccardi@my.bcit.ca>
2017-03-21 12:01:58 -0700

    Added draft canvas background

--- custom-box.js
+++ custom-box.js
@@ -1,108 +1,107 @@
-
 // Set up our canvas
 
-var canvasWidth  = 400;
+var canvasWidth = 400;
 var canvasHeight = 400;
-var box = {w: 140, h: 60, d: 90, color: '#494f55'};
-var wheelSize = {w:22, h:22};
+var box = { w: 140, h: 60, d: 90, color: '#ff0000' };
+var wheelSize = { w: 22, h: 22 };
 
-var boxWidthInput, boxHeightInput,boxDepthInput,boxColorInput, boxWidthLabel, boxHeightLabel, boxDepthLabel;
+var boxWidthInput, boxHeightInput, boxDepthInput, boxColorInput, boxWidthLabel, boxHeightLabel, boxDepthLabel;
 
 jQuery(document).ready(function($) {
     // Code that uses jQuery's $ can follow here.
 
-var canvas = $('<canvas/>',{'class':'custom-box-canvas',id: 'custom-box-canvas'}).prop({width: canvasWidth,height: canvasHeight});
+    var canvas = $('<canvas/>', { 'class': 'custom-box-canvas', id: 'custom-box-canvas' }).prop({ width: canvasWidth, height: canvasHeight });
     $('#custom-box-panel').append(canvas);
-  	var ctx = canvas[0].getContext('2d');
-  // Pick out the form elements for easy access later
-  	console.log("box", box);
-  	boxWidthInput = $('#boxWidth');
-  	boxHeightInput = $('#boxHeight');
-  	boxDepthInput = $('#boxDepth');
-  	boxColorInput = $('#boxColor');
+    var ctx = canvas[0].getContext('2d');
+    // Pick out the form elements for easy access later
+    console.log("box", box);
+    boxWidthInput = $('#boxWidth');
+    boxHeightInput = $('#boxHeight');
+    boxDepthInput = $('#boxDepth');
+    boxColorInput = $('#boxColor');
     boxWidthLabel = $('#boxWidthLabel');
     boxHeightLabel = $('#boxHeightLabel');
     boxDepthLabel = $('#boxDepthLabel');
 
-  	draw(ctx);
-  	boxWidthInput.on("input change",function() {draw(ctx);});
-  	boxHeightInput.on("input change",function() {draw(ctx);});
-  	boxDepthInput.on("input change",function() {draw(ctx);});
-  	//boxColorInput.change(function() {draw(ctx);});
-    $('.box-color-btn').click(function(event){
-      event.preventDefault();
-      box.color = $(this).attr('box-color');
-      draw(ctx);
+    draw(ctx);
+    boxWidthInput.on("input change", function() { draw(ctx); });
+    boxHeightInput.on("input change", function() { draw(ctx); });
+    boxDepthInput.on("input change", function() { draw(ctx); });
+    //boxColorInput.change(function() {draw(ctx);});
+    $('.box-color-btn').click(function(event) {
+        event.preventDefault();
+        box.color = $(this).attr('box-color');
+        draw(ctx);
     });
 
-	$('.box-wheel-btn').click(function(event){
-      event.preventDefault();
-      box.wheel = $(this).attr('box-wheel');
-	  console.log("box", box);
-      draw(ctx);
+    $('.box-wheel-btn').click(function(event) {
+        event.preventDefault();
+        box.wheel = $(this).attr('box-wheel');
+        console.log("box", box);
+        draw(ctx);
     });
 
-	});
+});
 
 // Animation function
-var draw = function(ctx){
-  // clear the canvas
-	ctx.globalCompositeOperation='destination-over';
-   ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+var draw = function(ctx) {
+    // clear the canvas
+    ctx.globalCompositeOperation = 'destination-over';
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
-	box.w = boxWidthInput.val();
-	box.h = boxHeightInput.val();
-	box.d = boxDepthInput.val();;
-	//box.color = boxColorInput.val();
-  boxWidthLabel.text(box.w);
-  boxHeightLabel.text(box.h);
-  boxDepthLabel.text(box.d);
+    box.w = boxWidthInput.val();
+    box.h = boxHeightInput.val();
+    box.d = boxDepthInput.val();;
+    //box.color = boxColorInput.val();
+    boxWidthLabel.text(box.w);
+    boxHeightLabel.text(box.h);
+    boxDepthLabel.text(box.d);
 
     // draw the wheels
-  if(typeof box.wheel != 'undefined'){
-		drawWheel(ctx, canvasWidth/2,
-    canvasHeight/2 + box.h/2,
-    Number(box.w),
-    Number(box.d),
-    Number(box.h));
-	}
- 
-  // draw the cube
-	drawCube(ctx,
-    canvasWidth/2,
-    canvasHeight/2 + box.h/2,
-    Number(box.w),
-    Number(box.d),
-    Number(box.h),
-    box.color
-  );
+    if (typeof box.wheel != 'undefined') {
+        drawWheel(ctx, canvasWidth / 2,
+            canvasHeight / 2 + box.h / 2,
+            Number(box.w),
+            Number(box.d),
+            Number(box.h));
+    }
+
+    // draw the cube
+    drawCube(ctx,
+        canvasWidth / 2,
+        canvasHeight / 2 + box.h / 2,
+        Number(box.w),
+        Number(box.d),
+        Number(box.h),
+        box.color
+    );
 
 }
 
-var drawWheel = function(ctx,x, y, wx, wy, h){
-	var wheel = new Image();
-	wheel.onload = function() {
-		console.log("wheel", wheel);
-		ctx.drawImage(wheel, x - wx, y - wx * 0.5);
-		ctx.drawImage(wheel,x-wheelSize.w*1.5, y-wheelSize.h*0.5);
-		ctx.drawImage(wheel,x + wy -wheelSize.w, y - wy * 0.5);
-	}
-	wheel.src = "/DinosaursDemo/wp-content/plugins/custom-box/assets/"+box.wheel+".svg";
+var drawWheel = function(ctx, x, y, wx, wy, h) {
+    var wheel = new Image();
+    wheel.onload = function() {
+        console.log("wheel", wheel);
+        ctx.drawImage(wheel, x - wx, y - wx * 0.5);
+        ctx.drawImage(wheel, x - wheelSize.w * 1.5, y - wheelSize.h * 0.5);
+        ctx.drawImage(wheel, x + wy - wheelSize.w, y - wy * 0.5);
+    }
+    wheel.src = "./assets/" + box.wheel + ".svg";
 };
 // Colour adjustment function
 // Nicked from http://stackoverflow.com/questions/5560248
 function shadeColor(color, percent) {
-  color = color.substr(1);
-  var num = parseInt(color, 16),
-    amt = Math.round(2.55 * percent),
-    R = (num >> 16) + amt,
-    G = (num >> 8 & 0x00FF) + amt,
-    B = (num & 0x0000FF) + amt;
-  return '#' + (0x1000000 + (R < 255 ? R < 1 ? 0 : R : 255) * 0x10000 + (G < 255 ? G < 1 ? 0 : G : 255) * 0x100 + (B < 255 ? B < 1 ? 0 : B : 255)).toString(16).slice(1);
+    color = color.substr(1);
+    var num = parseInt(color, 16),
+        amt = Math.round(2.55 * percent),
+        R = (num >> 16) + amt,
+        G = (num >> 8 & 0x00FF) + amt,
+        B = (num & 0x0000FF) + amt;
+    return '#' + (0x1000000 + (R < 255 ? R < 1 ? 0 : R : 255) * 0x10000 + (G < 255 ? G < 1 ? 0 : G : 255) * 0x100 + (B < 255 ? B < 1 ? 0 : B : 255)).toString(16).slice(1);
 }
 
 // Draw a cube to the specified specs
-var drawCube = function(ctx,x, y, wx, wy, h, color) {
+var drawCube = function(ctx, x, y, wx, wy, h, color) {
 
     ctx.beginPath();
     ctx.moveTo(x, y);
@@ -136,4 +135,4 @@ var drawCube = function(ctx,x, y, wx, wy, h, color) {
     ctx.strokeStyle = shadeColor(color, 60);
     ctx.stroke();
     ctx.fill();
-  }
+}
